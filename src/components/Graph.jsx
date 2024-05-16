29bc2725618fcbbdd1179ecdb8653df0c22da7bb
johnnyyork13 <johnnyyork13@hotmail.com>
2024-05-15 22:14:33 -0400

    add stocks

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -7,6 +7,8 @@ export default function Graph(props) {
     const [points, setPoints] = useState([]);
     const [johnnyScore, setJohnnyScore] = useState(0);
     const [allisonScore, setAllisonScore] = useState(0);
+    const [stockPrice, setStockPrice] = useState(0);
+    const BASE_STOCK_PRICE = 137.49;
 
     useEffect(() => {
         try {
@@ -25,24 +27,37 @@ export default function Graph(props) {
                     }
                 }).catch((err) => console.log(err));
             }
+            // async function getStockPrice() {
+            //     console.log("getting stock");
+            //     const url = "https://financialmodelingprep.com/api/v3/discounted-cash-flow/YUM?apikey=XlqZqlHkUIWCy3WtndRELL3S2yIOfGaQ";
+            //     await fetch(url).then((res) => res.json())
+            //     .then((res) => {
+            //         setStockPrice(res[0]["Stock Price"]);
+            //     }).catch((err) => console.log(err));
+            // }
+            // getStockPrice();
+            setStockPrice(137.49)
             getPoints();
         } catch(err) {
             console.log(err);
         }
     }, [])  
 
-    console.log(points);
+    console.log(points, stockPrice);
 
     return (
         <GraphContainer> 
+            <StockPriceContainer>
+                <p>Taco Bell Current Stock Price: {stockPrice}</p>
+            </StockPriceContainer>
             <GraphBody>
                 <GraphSidebar>
                     <p>Johnny</p>
                     <p>Allison</p>
                 </GraphSidebar>
                 <ScoreContainer>
-                    <JohnnyScore $score={johnnyScore}></JohnnyScore>
-                    <AllisonScore $score={allisonScore}></AllisonScore>
+                    <JohnnyScore $score={johnnyScore * (stockPrice / BASE_STOCK_PRICE)}></JohnnyScore>
+                    <AllisonScore $score={allisonScore * (stockPrice / BASE_STOCK_PRICE)}></AllisonScore>
                 </ScoreContainer>    
             </GraphBody>
             <GraphFooter>
@@ -56,7 +71,6 @@ export default function Graph(props) {
 }
 
 const GraphContainer = styled.div`
-    border: 1px solid black;
     padding: 10px;
     width: 500px;
     height: 200px;
@@ -73,7 +87,12 @@ const HeaderSection = styled.div`
 
 `
 
+const StockPriceContainer = styled.div`
+
+`
+
 const GraphBody = styled.div`
+    border: 1px solid black;
     height: 170px;
     display: flex;
     justify-content: space-between;
